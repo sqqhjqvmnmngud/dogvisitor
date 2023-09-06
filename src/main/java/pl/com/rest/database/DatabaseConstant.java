685cd894b2d153f8462c58af0f3bca331b6c68b0
Mateusz Skowron <skowronmatt@gmail.com>
2023-09-06 21:46:32 +0200

    Changed db server address

--- src/main/java/pl/com/rest/database/DatabaseConstant.java
+++ src/main/java/pl/com/rest/database/DatabaseConstant.java
@@ -5,7 +5,7 @@ package pl.com.rest.database;
  */
 public class DatabaseConstant {
 
-    public static final String HOST = "localhost";
+    public static final String HOST = "172.17.0.1";
     public static final int PORT = 27017;
     public static final String DATABASE = "dogvisitor";
     public static final String USER_NAME = "dog";
